35b7b3954516c733cf6b8b910f6f968ad3e9560a
edgvac2298 <edgardovac2298@gmail.com>
2022-08-13 13:49:58 -0500

    Callback + setTimeout

--- src/callback/index.js
+++ src/callback/index.js
@@ -7,3 +7,13 @@ function calc(num1, num2, callback) {
 }
 
 console.log(calc(8, 4, sum));
+
+setTimeout(function() {
+    console.log('Hola js');
+}, 5000)
+
+function greetings(name) {
+    console.log(`Hello ${name}`);
+}
+
+setTimeout(greetings, 2000, 'Edgardo');
